85d455aa6a24ba6a0e7d1e52ebe20f0fb82a25b1
Na Song <quic_nasong@quicinc.com>
2024-08-06 14:57:31 +0800

    qrb_manager_inference:  1. add copyright lines for passing repolinter check

--- qrb_inference_manager/include/qrb_inference_manager.hpp
+++ qrb_inference_manager/include/qrb_inference_manager.hpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
 #ifndef QRB_INFERENCE_MANAGER_HPP_
 #define QRB_INFERENCE_MANAGER_HPP_
 
