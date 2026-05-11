a29bb450c7acc13c7c7faf9a94b08ba765cd41ca
Na Song <nasong@qti.qualcomm.com>
2026-05-11 14:47:55 +0800

    fix: wrong graph name
    Signed-off-by: Na Song <nasong@qti.qualcomm.com>

--- qrb_inference_manager/src/qnn_inference/qnn_inference.cpp
+++ qrb_inference_manager/src/qnn_inference/qnn_inference.cpp
@@ -361,12 +361,13 @@ StatusCode QnnInference::copy_graph_info(T graph_info_from_binary)
     }
 
     graph_info_dst->graph_name =
-        (char *)malloc(sizeof(char) * strlen(graph_info_from_binary.graphName));
+        (char *)malloc(sizeof(char) * (strlen(graph_info_from_binary.graphName) + 1));
     if (nullptr == graph_info_dst->graph_name) {
       return StatusCode::FAILURE;
     }
     memcpy(graph_info_dst->graph_name, graph_info_from_binary.graphName,
         strlen(graph_info_from_binary.graphName));
+    graph_info_dst->graph_name[strlen(graph_info_from_binary.graphName)] = '\0';
 
     auto set_up_tensors_info = [](const Qnn_Tensor_t * src, const uint32_t cnt,
                                    Qnn_Tensor_t *& dst) {
